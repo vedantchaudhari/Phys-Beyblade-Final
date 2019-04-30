0ba2b4ed34c57233a7742c3020a1061eeddc551b
aaron.hamilton <aaron.hamilton@mymail.champlain.edu>
2019-04-30 12:32:58 -0400

    created dll project

---
+++ BeybladePlugin/BeybladePlugin/BeybladePlugin.cpp
@@ -0,0 +1,6 @@
+// BeybladePlugin.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+
+
---
+++ BeybladePlugin/BeybladePlugin/Serialize.h
@@ -0,0 +1,9 @@
+/*
+These functions will be used to 
+Serialize data read in from Unity
+Into data that can be read/used/manipulated in C++
+*/
+
+#pragma once
+
+#include "stdafx.h"
