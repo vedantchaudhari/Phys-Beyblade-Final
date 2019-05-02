41bc76755cc6816d4ec10e8ad19267395b318a07
Vedant Chaudhari <vedantchaudhari@outlook.com>
2019-05-02 19:28:49 -0400

    Added models to scene, Added rotational physics, Adding marshalling code to work with c++

---
+++ Assets/_Code/BeybladeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class BeybladeController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody mRigidbody;
+    [SerializeField]
+    private Vector3 mCenterOfMass;
+
+    public void Launch()
+    {
+        mRigidbody.isKinematic = false;
+        mRigidbody.AddTorque(new Vector3(0, 2000 * 100, 0));
+        mRigidbody.AddForce(Vector3.forward * 1000);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRigidbody = GetComponent<Rigidbody>();
+        mRigidbody.isKinematic = true;
+        mRigidbody.centerOfMass = mCenterOfMass;
+
+        Launch();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/_Code/PhysicsDataStructs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PhysicsDataStructs : MonoBehaviour
+{
+    [StructLayout(LayoutKind.Sequential, Size = 12), System.Serializable]
+    public struct RigidBodyData
+    {
+        // Vector 3 position
+        [MarshalAsAttribute(UnmanagedType.R4, SizeConst = 4)]
+        public float x;
+        [MarshalAsAttribute(UnmanagedType.R4, SizeConst = 4)]
+        public float y;
+        [MarshalAsAttribute(UnmanagedType.R4, SizeConst = 4)]
+        public float z;
+    }
+}
