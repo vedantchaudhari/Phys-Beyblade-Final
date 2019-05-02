d6c4c619386609404e018ea606f77384df4724d8
Vedant Chaudhari <vedantchaudhari@outlook.com>
2019-05-02 19:39:22 -0400

    added angular dynamics

--- Assets/_Code/PhysicsDataStructs.cs
+++ Assets/_Code/PhysicsDataStructs.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class PhysicsDataStructs : MonoBehaviour
 {
-    [StructLayout(LayoutKind.Sequential, Size = 12), System.Serializable]
+    [StructLayout(LayoutKind.Sequential, Size = 20), System.Serializable]
     public struct RigidBodyData
     {
         // Vector 3 position
@@ -15,5 +15,10 @@ public class PhysicsDataStructs : MonoBehaviour
         public float y;
         [MarshalAsAttribute(UnmanagedType.R4, SizeConst = 4)]
         public float z;
+
+        [MarshalAsAttribute(UnmanagedType.R4, SizeConst = 4)]
+        public float angle;
+        [MarshalAsAttribute(UnmanagedType.R4, SizeConst = 4)]
+        public float angularVelocity;
     }
 }
